76f8a7159dfb3b8951ea06ee4630560d8e40815f
Shawn Rutledge <s@ecloud.org>
2026-06-09 20:01:51 +0200

    Move Moody's riscv64 gefs atomics to libc
    Added in 0091504b6ad7b992a26c59aaaa59dac9887fabc7 but left unused.
    Let's see if the tests pass.
    
    As for the other platforms, 32-bit operations are in atomic.s
    and 64-bit in atomic64.s.

---
+++ sys/src/libc/riscv64/atomic64.s
@@ -0,0 +1,52 @@
+#include <atom.h>
+
+#define ARG 8
+
+/*  get variants */
+TEXT agetv+0(SB),1,$0
+TEXT agetp+0(SB),1,$0
+	FENCE_RW
+	LRD(ARG, ARG)
+	FENCE_RW
+	RET
+
+/*  set variants */
+TEXT asetv+0(SB),1,$0
+TEXT asetp+0(SB),1,$0
+	MOV	val+XLEN(FP), R12
+	FENCE_RW
+	AMOD(Amoswap, AQ|RL, 12, ARG, 10)
+	FENCE_RW
+	MOV	R10, RARG
+	RET
+
+/*  inc variants */
+TEXT aincv+0(SB),1,$0
+TEXT aincp+0(SB),1,$0
+	MOV	$1, R9
+	FENCE_RW	/* flush changes to ram in case releasing a lock */
+	/* after: value before add in R10, value after add in memory */
+	AMOD(Amoadd, AQ|RL, 9, ARG, 10)
+	FENCE_RW
+	ADDW	$1, R10, RARG		/* old value ±1 for ainc/adec */
+	RET
+
+/*  cas variants */
+TEXT acasv+0(SB),1,$0
+TEXT acasp+0(SB),1,$0
+	MOV	ov+XLEN(FP), R12
+	MOV	nv+(2*XLEN)(FP), R13
+	MOV	R0, R11		/* default to failure */
+	FENCE_RW
+spincasp:
+	LRD(ARG, 14)		/* (RARG) -> R14 */
+	BNE	R12, R14, fail
+	FENCE_RW
+	SCD(13, ARG, 14)	/* R13 -> (RARG) maybe, R14=0 if ok */
+	BNE	R14, spincasp	/* R14 != 0 means store failed */
+ok:
+	MOV	$1, R11
+fail:
+	FENCE_RW
+	MOV	R11, RARG
+	RET
